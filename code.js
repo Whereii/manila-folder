44c55cec826eb6ee80a5e2f94acedb32767163b0
JoshuaMcCausey <joshuamccausey@gmail.com>
2022-06-01 16:17:51 -0400

    Create closing animation and touch up how it all works

--- code.js
+++ code.js
@@ -1,5 +1,12 @@
 let frontFile = document.querySelector('.front-folder');
+let closeFile = document.querySelector('.close')
 
 frontFile.addEventListener('click', function (e) {
+    frontFile.classList.remove('flip-closed')
     e.target.classList.add('flip-front');
 });
+
+closeFile.addEventListener('click', function (e) {
+    frontFile.classList.add('flip-closed')
+    frontFile.classList.remove('flip-front')
+})
