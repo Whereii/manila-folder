ab8790ded16375f38a15be4d8cd4becc88b9120d
JoshuaMcCausey <joshuamccausey@gmail.com>
2022-06-03 21:53:05 -0400

    Move close button onto tab and start styling pages

--- code.js
+++ code.js
@@ -1,12 +1,15 @@
 let frontFile = document.querySelector('.front-folder');
-let closeFile = document.querySelector('.close')
-let body = document.querySelector('.body')
+let closeFile = document.querySelector('.close');
+let body = document.querySelector('.body');
+let close = document.querySelector('.close');
 
 frontFile.addEventListener('click', function (e) {
     frontFile.classList.remove('flip-closed');
     e.target.classList.add('flip-front');
     body.classList.remove('send-back');
     body.classList.add('bring-forward');
+    close.classList.remove('btn-fade');
+    close.classList.add('btn-appear');
 });
 
 closeFile.addEventListener('click', function (e) {
@@ -14,4 +17,6 @@ closeFile.addEventListener('click', function (e) {
     frontFile.classList.remove('flip-front');
     body.classList.remove('bring-forward');
     body.classList.add('send-back');
-})
+    close.classList.remove('btn-appear');
+    close.classList.add('btn-fade');
+});
