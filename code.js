b84cb35558f7ea4a1ec983d43e30a691bfb0e83c
JoshuaMcCausey <joshuamccausey@gmail.com>
2022-06-03 20:55:03 -0400

    Add zoom animation to body of document when opened

--- code.js
+++ code.js
@@ -1,12 +1,17 @@
 let frontFile = document.querySelector('.front-folder');
 let closeFile = document.querySelector('.close')
+let body = document.querySelector('.body')
 
 frontFile.addEventListener('click', function (e) {
-    frontFile.classList.remove('flip-closed')
+    frontFile.classList.remove('flip-closed');
     e.target.classList.add('flip-front');
+    body.classList.remove('send-back');
+    body.classList.add('bring-forward');
 });
 
 closeFile.addEventListener('click', function (e) {
-    frontFile.classList.add('flip-closed')
-    frontFile.classList.remove('flip-front')
+    frontFile.classList.add('flip-closed');
+    frontFile.classList.remove('flip-front');
+    body.classList.remove('bring-forward');
+    body.classList.add('send-back');
 })
